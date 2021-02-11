7803017cf6f3d385a654e04c3853ae34205c2006
Rafael Leite <rl.oliveira321@gmail.com>
2021-02-11 01:32:12 -0300

    Exception handling for delete (referentuak integrity)

---
+++ SalesWebMVC/Services/Exceptions/IntegrityExcepetion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class IntegrityExcepetion : ApplicationException
+    {
+        public IntegrityExcepetion(string message) : base(message)
+        {
+
+        }
+    }
+}
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -31,9 +31,16 @@ namespace SalesWebMVC.Services
         }
         public async Task RemoveAsync(int id)
         {
+            try
+            {
             var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
             await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityExcepetion("Cant't delete seller because he/she has sales");
+            }
         }
         public async Task UpdateAsync(Seller obj)
         {
